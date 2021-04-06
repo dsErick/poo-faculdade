449fa0835258be4b805d8c279ac20f24d819bb0c
Erick Silva <ericksilvablue@gmail.com>
2021-04-05 22:04:40 -0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Erick
+ */
+public class App {
+    /**
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
